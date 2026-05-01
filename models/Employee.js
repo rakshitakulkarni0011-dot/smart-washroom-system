aa8231f09d4f5afc7d317ea956a412ddbe14e227
Prajwal Kulkarni <128566438+Prajwalkulkarni766@users.noreply.github.com>
2026-05-01 11:08:33 +0530

    https://github.com/rakshitakulkarni0011-dot/smart-washroom-system

---
+++ models/Employee.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  phone: { type: String, required: true, unique: true },
+  password: String,
+  role: String,
+  available: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
